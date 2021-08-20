662729393d98522b3461a2db841fd12efd60d64d
Sajan4072 <rai4072@gmail.com>
2021-08-20 20:03:57 +0545

    all fixes till this point

--- Dajax/Dajax/urls.py
+++ Dajax/Dajax/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ajaxapp import views
+
+from django.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
+    path('',include('ajaxapp.urls')),
+
+    
 ]
--- Dajax/ajaxapp/views.py
+++ Dajax/ajaxapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from . forms import StudentRegistration
+from .forms import StudentRegistration
 
 # Create your views here.
 def home(request):
-    form=StudentRegistration() #creating an empty form
-    return render(request,'home.html',{'form':form})
-
+    #creating an empty form
     
+    form=StudentRegistration() 
+    return render(request,'ajaxapp/home.html',{'form':form})
+
+
 
 
