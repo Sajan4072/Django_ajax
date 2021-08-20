e35238e55956868039e8c002cf2bc06eb2a429a5
Sajan4072 <rai4072@gmail.com>
2021-08-20 17:18:15 +0545

    template created and writing home view

--- Dajax/ajaxapp/views.py
+++ Dajax/ajaxapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from . forms import StudentRegistration
 
 # Create your views here.
+def home(request):
+    form=StudentRegistration() #creating an empty form
+    return render(request,'home.html',{'form':form})
+
+    
+
+
