8616f0d212f6eaa88d3e1f8380641df1e18bc9ec
Sajan4072 <rai4072@gmail.com>
2021-08-21 20:34:37 +0545

    data fetched from database using User.objects.all()

--- Dajax/ajaxapp/views.py
+++ Dajax/ajaxapp/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .forms import StudentRegistration
+from .models import User
 
 # Create your views here.
 def home(request):
     #creating an empty form
     
-    form=StudentRegistration() 
-    return render(request,'ajaxapp/home.html',{'form':form})
+    form=StudentRegistration()
+    stud=User.objects.all() 
+    return render(request,'ajaxapp/home.html',{'form':form,'stu':stud})
 
 
 
